0c3b18aea58ae52b701b4526a12bf1c4aa2360f8
kenblocklive <kenblock@live.ru>
2023-10-29 20:02:15 +0400

    js logic workout

--- index.js
+++ index.js
@@ -7,3 +7,70 @@
 // TODO создать функцию, которая позволит изменить дату тренировка.
 // TODO создать функцию, которая позвонли удалить все записи.
 
+// объявил переменную и записал в нее массив
+// в котором мы будем хранить результаты тренировок
+const workoutResults = [];
+//создал функцию для добавления новых результатов тренировок
+function addResult(weight, link) {
+  //шаблон тренировки
+  const result = {
+    //создаем генератор id  для каждой тренировки
+    id: +Math.random().toString().slice(2),
+    weight: weight,
+    // стандартная функция для получения текущей даты
+    date: new Date().toLocaleDateString(),
+    imageLink: link,
+  };
+  workoutResults.push(result);
+}
+addResult(90, "/wefwef");
+// создал функцию для удаления тренировок из массива
+function removeResult(id) {
+  //используем этот метод, чтобы найти
+  //индекс нужного элемента в массиве
+  const resultDeleteIndex = workoutResults.findIndex(
+    // перебираем все элементы массива и записываем их
+    //в переменную element, потом сравниваем id элементов
+    // массива и id которым мы передаем в функцию
+    // по итогу в переменной resultDeleteIndex будет индекс
+    // элемента который нам надо удалить
+    (element) => element.id === id
+  );
+  // с помощью функции splice мы удаляем нашу тренирвоку из массива
+  // зная индекс этой тренировки
+  workoutResults.splice(resultDeleteIndex, 1);
+}
+//создаем функцию для       изменения веса у нужной тренировки
+function changeWeight(id, weight) {
+  // с помощью find  мы ищем нужную тренировку в массиве
+  // сравнивая id кждого элемента с id  который мы передаем в функцию
+  const changeItem = workoutResults.find((item) => item.id === id);
+  //в найденном элементе обращаемся к его полю weight
+  // и присваиваем ему новое значение, которое передаем через параметр функции
+  changeItem.weight = weight;
+}
+//создаем функцию для       изменения ссылки на картинку у нужной тренировки
+function changeImageLink(id, imageLink) {
+  // с помощью find  мы ищем нужную тренировку в массиве
+  // сравнивая id каждого элемента с id  который мы передаем в функцию
+  const changeItem = workoutResults.find((item) => item.id === id);
+  //в найденном элементе обращаемся к его полю imageLink
+  // и присваиваем ему новое значение, которое передаем через параметр функции
+  changeItem.imageLink = imageLink;
+}
+//создаем функцию для  изменения даты у нужной тренировки
+function changeDate(id, newDate) {
+  // с помощью find  мы ищем нужную тренировку в массиве
+  // сравнивая id каждого элемента с id  который мы передаем в функцию
+  const changeItem = workoutResults.find((item) => item.id === id);
+  //в найденном элементе обращаемся к его полю date
+  // и присваиваем ему новое значение, которое передаем через параметр функции
+  changeItem.date = newDate;
+}
+
+//очищаем массив с нашими тренировками
+function removeAllResults() {
+  workoutResults.length = 0;
+}
+
+console.log(workoutResults);
